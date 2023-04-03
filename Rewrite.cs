b136934e40543dda1583fc8bdaea40f212db2a5f
SALMON <shannanoneill67@gmail.com>
2023-04-03 15:56:08 +0100

    I want to rewrite this and add even better features :)))

---
+++ Rewrite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleAnimalsImproved
+{
+    internal class Rewrite
+    {
+        // bebe and poppy fight hopes and dreams!
+
+        // variables : money, health of pets, damage taken, number of wins, amount bet
+
+        // pets: strength levels, health levels, defense levels, speed levels for dodge
+
+        // have a list of pets, and pick 2 of them at random to fight at the start of the round
+
+        // random event chance, inputting bonus lines
+        // e.g "Bebe slipped on a banana, and fell onto Mars! Bebe's attack was doubled!"
+
+        // counting in memory the #1, #2 , #3 champions from all games with ascii art
+
+        // 
+    }
+}
